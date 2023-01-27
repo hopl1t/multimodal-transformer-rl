3acba86f40c1a967c751fb6ce48ec4b95cbf17b3
Nir Weingarten <nirzvi89@gmail.com>
2023-01-27 12:53:25 +0200

    added new trainer
    Signed-off-by: Nir Weingarten <nirzvi89@gmail.com>

--- src/train.py
+++ src/train.py
@@ -13,9 +13,6 @@ import torch.optim as optim
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
 
-from Minecraft import Config
-from Minecraft import Minecraft
-
 from stable_baselines3.common.atari_wrappers import (  # isort:skip
     ClipRewardEnv,
     EpisodicLifeEnv,
@@ -23,14 +20,33 @@ from stable_baselines3.common.atari_wrappers import (  # isort:skip
     MaxAndSkipEnv,
     NoopResetEnv,
 )
-
+from agents import GymAgent, MinecraftAgent, OldAgent
+from Minecraft import Config
+from Minecraft import Minecraft
 from utils import save_run, load_run, parse_args, make_minecraft_env, layer_init
-from agents import MinecraftAgent, GymAgent, OldAgent
 
 
 def make_env(env_id, seed, idx, capture_video, run_name):
-    if not env_id:
-        def thunk():
+    def thunk_atari():
+        env = gym.make(env_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = EpisodicLifeEnv(env)
+        if "FIRE" in env.unwrapped.get_action_meanings():
+            env = FireResetEnv(env)
+        env = ClipRewardEnv(env)
+        env = gym.wrappers.ResizeObservation(env, (84, 84))
+        env = gym.wrappers.GrayScaleObservation(env)
+        env = gym.wrappers.FrameStack(env, 1)
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+    def thunk_minecraft():
             env = Minecraft(env_id=idx)
             env.seed(seed)
             env = gym.wrappers.RecordEpisodeStatistics(env)
@@ -40,84 +56,18 @@ def make_env(env_id, seed, idx, capture_video, run_name):
             _ = env.reset()
             # env = ClipRewardEnv(env)  # We probably shouldnt use this in minecraft, the reward normalization is enough
             return env
+    if env_id == 'minecraft':
+        thunk = thunk_minecraft
     else:
-        def thunk():
-            env = gym.make(env_id)
-            env = gym.wrappers.RecordEpisodeStatistics(env)
-            if capture_video:
-                if idx == 0:
-                    env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-            env = NoopResetEnv(env, noop_max=30)
-            env = MaxAndSkipEnv(env, skip=4)
-            env = EpisodicLifeEnv(env)
-            if "FIRE" in env.unwrapped.get_action_meanings():
-                env = FireResetEnv(env)
-            env = ClipRewardEnv(env)
-            env = gym.wrappers.ResizeObservation(env, (84, 84))
-            env = gym.wrappers.GrayScaleObservation(env)
-            env = gym.wrappers.FrameStack(env, 1)
-            env.seed(seed)
-            env.action_space.seed(seed)
-            env.observation_space.seed(seed)
-            return env
+        thunk = thunk_atari
     return thunk
 
 
 if __name__ == "__main__":
     args = parse_args()
-    if Config.USE_AUDIO:
+    if Config.USE_AUDIO and (args.env_id == 'minecraft'):
         print('### 🔊 USING AUDIO 🔊 ###')
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-
-    # TRY NOT TO MODIFY: seeding
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
-    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-
-    # envs = make_env(args.seed, 0, args.capture_video, run_name)()
-
-    envs = gym.vector.SyncVectorEnv(
-        [make_env(args.env_id, args.seed + i, i, args.capture_video, run_name)
-         for i in range(args.num_envs)]
-    )
-
-    if args.env_id.lower() == "minecraft":
-        agent = MinecraftAgent(envs, device, conv_type=args.conv_size, attn_type=args.attn_type, fusion_type=args.fusion_type).to(device)
-    else:
-        # agent = GymAgent(envs, conv_type=args.conv_size).to(device)
-        agent = OldAgent(envs).to(device)
-    
-    optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
-
-    # ALGO Logic: Storage setup
-    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
-    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
-    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
-
-    # TRY NOT TO MODIFY: start the game
-    global_step = 0
-    start_time = time.time()
-    next_obs = torch.Tensor(envs.reset()).to(device)
-    next_done = torch.zeros(args.num_envs).to(device)
-    next_lstm_state = (
-        torch.zeros(agent.lstm.num_layers, args.num_envs, agent.lstm.hidden_size).to(device),
-        torch.zeros(agent.lstm.num_layers, args.num_envs, agent.lstm.hidden_size).to(device),
-    )  # hidden and cell states (see https://youtu.be/8HyCNIVRbSU)
-    num_updates = args.total_timesteps // args.batch_size
-
-    episode_rewards = np.zeros((args.max_episode_len, args.num_envs))
-    episode = 0
-    initial_update = 1
-
-    if args.load_from:
-        agent, run_name, args, optimizer, global_step, episode, initial_update = load_run(args.load_from)
-
     if args.track:
         import wandb
 
@@ -133,12 +83,63 @@ if __name__ == "__main__":
     writer = SummaryWriter(f"runs/{run_name}")
     writer.add_text(
         "hyperparameters",
-        "|param|value|\n|-|-|\n%s" % (
-            "\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
     )
-    
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.env_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
+    )
+
+    # agent = Agent(envs).to(device)
+    if args.env_id == 'minecraft':
+        agent = MinecraftAgent(envs, device).to(device)
+    else:
+        agent = OldAgent(envs).to(device)
+    optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
+
+    # ALGO Logic: Storage setup
+    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
+    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+
+    episode_rewards = np.zeros((args.max_episode_len, args.num_envs))
+    episode = 0
+    initial_update = 1
+
+    if args.load_from:
+        agent, run_name, args, optimizer, global_step, episode, initial_update = load_run(args.load_from)
+        agent.to(device)
+
+    # TRY NOT TO MODIFY: start the game
+    global_step = 0
+    start_time = time.time()
+    next_obs = torch.Tensor(envs.reset()).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+    next_lstm_state = (
+        torch.zeros(agent.lstm.num_layers, args.num_envs, agent.lstm.hidden_size).to(device),
+        torch.zeros(agent.lstm.num_layers, args.num_envs, agent.lstm.hidden_size).to(device),
+    )  # hidden and cell states (see https://youtu.be/8HyCNIVRbSU)
+    num_updates = args.total_timesteps // args.batch_size
+
     for update in range(initial_update, num_updates + 1):
         initial_lstm_state = (next_lstm_state[0].clone(), next_lstm_state[1].clone())
+        # Annealing the rate if instructed to do so.
+        if args.anneal_lr:
+            frac = 1.0 - (update - 1.0) / num_updates
+            lrnow = frac * args.learning_rate
+            optimizer.param_groups[0]["lr"] = lrnow
 
         for step in range(0, args.num_steps):
             global_step += 1 * args.num_envs
@@ -154,31 +155,38 @@ if __name__ == "__main__":
 
             # TRY NOT TO MODIFY: execute the game and log data.
             next_obs, reward, done, info = envs.step(action.cpu().numpy())
-            info = [info]  # original implementation had many envs
             rewards[step] = torch.tensor(reward).to(device).view(-1)
-            episode_rewards[step] = reward
-            next_obs, next_done = torch.Tensor(next_obs).to(
-                device), torch.Tensor(done).to(device)
+            # episode_rewards[step] = reward.copy()
+            next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
 
-            if any(done):
-                done_episodes = np.atleast_1d(done).nonzero()[0]
-                # episode_reward = episode_rewards.sum()  # sums over all envs
-                # takes only the some of the finished episode
-                done_episode_rewards = episode_rewards.sum(axis=0)[done_episodes]
-                episode_rewards[:, done_episodes] = np.zeros_like(episode_rewards[:, done_episodes])
-                # episode_rewards = np.zeros((args.max_episode_len, args.num_envs))
-                episode += len(done_episodes)
-                # Annealing the rate if instructed to do so.
-                for episode_reward in done_episode_rewards:
-                    if args.anneal_lr:
-                        frac = 1.0 - (episode - 1.0) / args.max_episodes
-                        lrnow = frac * args.learning_rate
-                        optimizer.param_groups[0]["lr"] = lrnow
-                    writer.add_scalar("charts/episodic_reward",
-                                    episode_reward, global_step)
-                    writer.add_scalar("charts/episodic_length", step, global_step)
+            for item in info:
+                if "episode" in item.keys():
                     if not (episode % args.print_interval):
-                        print(f"episode #{episode}, global_step={global_step}, episodic_return={episode_reward}, new lr={optimizer.param_groups[0]['lr']}")
+                        print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+                    break
+
+            # New metrics
+            # if any(done):
+            #     done_episodes = np.atleast_1d(done).nonzero()[0]
+            #     # episode_reward = episode_rewards.sum()  # sums over all envs
+            #     # takes only the some of the finished episode
+            #     done_episode_rewards = episode_rewards.sum(axis=0)[done_episodes]
+            #     episode_rewards[:, done_episodes] = np.zeros_like(episode_rewards[:, done_episodes])
+            #     # episode_rewards = np.zeros((args.max_episode_len, args.num_envs))
+            #     episode += len(done_episodes)
+            #     # Annealing the rate if instructed to do so.
+            #     for episode_reward in done_episode_rewards:
+            #         # if args.anneal_lr:
+            #         #     frac = 1.0 - (episode - 1.0) / args.max_episodes
+            #         #     lrnow = frac * args.learning_rate
+            #         #     optimizer.param_groups[0]["lr"] = lrnow
+            #         writer.add_scalar("charts/episodic_reward_new",
+            #                         episode_reward, global_step)
+            #         # if not (episode % args.print_interval):
+            #         #     print(f"episode #{episode}, global_step={global_step}, episodic_return={episode_reward}, new lr={optimizer.param_groups[0]['lr']}")
+
 
         # bootstrap value if not done
         with torch.no_grad():
--- src/utils.py
+++ src/utils.py
@@ -57,7 +57,7 @@ def parse_args():
         help="if toggled, cuda will be enabled by default")
     parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="if toggled, this experiment will be tracked with Weights and Biases")
-    parser.add_argument("--wandb-project-name", type=str, default="Minecraft",
+    parser.add_argument("--wandb-project-name", type=str, default="cleanRL",
         help="the wandb's project name")
     parser.add_argument("--wandb-entity", type=str, default=None,
         help="the entity (team) of wandb's project")
@@ -65,29 +65,29 @@ def parse_args():
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--env-id", type=str, default="minecraft",
+    parser.add_argument("--env-id", type=str, default="BreakoutNoFrameskip-v4",
         help="the id of the environment")
     parser.add_argument("--total-timesteps", type=int, default=10000000,
         help="total timesteps of the experiments")
-    parser.add_argument("--learning-rate", type=float, default=3e-4,
+    parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
     parser.add_argument("--num-envs", type=int, default=1,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
-    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
     parser.add_argument("--gamma", type=float, default=0.99,
         help="the discount factor gamma")
-    parser.add_argument("--gae-lambda", type=float, default=0.9,
+    parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
     parser.add_argument("--num-minibatches", type=int, default=1,
         help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=4,
+    parser.add_argument("--update-epochs", type=int, default=1,
         help="the K epochs to update the policy")
     parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles advantages normalization")
-    parser.add_argument("--clip-coef", type=float, default=0.25,
+    parser.add_argument("--clip-coef", type=float, default=0.1,
         help="the surrogate clipping coefficient")
     parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")

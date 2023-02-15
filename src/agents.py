fixed two bugs in vib: attention was doubled and kl was negative
Signed-off-by: Nir Weingarten <nirzvi89@gmail.com>

src/agents.py
@@ -657,13 +657,10 @@ class VIBAgent(nn.Module):
             x, 1, torch.tensor([0]).to(self.device)))
         audio_features = self.audio_net(torch.index_select(
             x, 1, torch.tensor([1]).to(self.device)))
-        if self.use_attn:
-            video_features, audio_features, attn_weights = self.attn(
-                video_features, audio_features, lstm_state)
 
         # attn
         if self.use_attn:
-            video_features, audio_features, _ = self.attn(video_features, audio_features, lstm_state)
+            video_features, audio_features, attn_weights = self.attn(video_features, audio_features, lstm_state)
 
         # Fusion
         if self.fusion_type == 'tensor':

src/casl_vib.py
@@ -226,7 +226,9 @@ if __name__ == "__main__":
                     v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
 
                 # Information loss
-                kl = 0.5 * torch.mean(mu.pow(2) + std.pow(2) - 2*std.log() - 1)
+                # normalization_loss = -0.5 * (1 + 2 * std.log() - mu.pow(2) - std.pow(2)).sum()  # From aml project
+                # return reconstruction_loss + beta * normalization_loss
+                kl = -0.5 * torch.mean(mu.pow(2) + std.pow(2) - 2*std.log() - 1)  # this is positive kl, needs to be negated for loss as we wan to increase kl
                 info_loss = args.ib_beta * kl
 
                 entropy_loss = entropy.mean()